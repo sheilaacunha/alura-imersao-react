7c6fd6952d4539a6a90369fbf7abbe8f34157d1b
sheilaacunha <103156674+sheilaacunha@users.noreply.github.com>
2022-11-14 17:31:42 -0300

    Update index.js

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import DarkModeSwitch from "./components/DarkModeswitch";
+import DarkModeSwitch from "./components/DarkModeSwitch";
 import Search from "./components/Search"
 
 const StyledMenu = styled.header`
